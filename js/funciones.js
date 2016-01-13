a2bac4a8de3ec4d2bf9bf5aeb835818b0d2d5249
Gildardo Adrian Maravilla Jacome <fulapol@gmail.com>
2016-01-13 17:57:02 -0600

    Agregado campo de explicaciones y niveles 1-5

--- js/funciones.js
+++ js/funciones.js
@@ -1,35 +1,79 @@
 $ = document.getElementById.bind(document);
-nivel = 0;
+var nivel = 0;
+var old, prev = '';
+var oldConsole = console;
+var oldAlert = alert;
 
 function html(txt) {
     $('html').textContent = txt;
 }
 
 function log(txt) {
-    $('log').textContent = txt;
+    if (prev.toString().search('console.log')>0) {
+        $('log').textContent = prev + "\n" + txt;
+    } else {
+        $('log').textContent = txt;
+    }
+    prev = txt;
+}
+
+function hooks() {
+    alert = function(s) {
+        if (s == document.cookie) {
+            $('code').innerHTML = '<a class="button" onclick="cargarNivel('+(nivel+1)+')">Siguiente nivel</a>';
+            oldAlert('Completado');
+            };
+        oldConsole.log(s);
+        };
+    console = {
+        log: function(s) {
+            log(s);
+            }
+        };
 }
 
 function check() {
     var input = $('input').value;
-    filter = eval('('+escaper[nivel]+')');
-    html(filter(input));
-    sandbox.document.open();
-    sandbox.alert = function(s) {
-        if (s == document.cookie) {
-            alert('Completado');
-            };
-        };
-    sandbox.console = {
-        log: function(s) {
-            log(s)
-            }
-        };
-    sandbox.document.write(filter(input));
-    sandbox.document.close();
-
+    if (old != input) {
+        old = input;
+    eval('('+
+        'function script(filtro, data) {'+
+        'hooks();'+
+        'filtrado = eval(\'(\'+escaper[nivel]+\')\')(input);'+
+        'html(filtrado);'+
+        'sandbox.document.open();'+
+        'sandbox.alert = function(s) {'+
+            'if (s == document.cookie) {'+
+                '$(\'code\').innerHTML = \'<a class="button" onclick="cargarNivel(\'+(nivel+1)+\')">Siguiente nivel</a>\';'+
+                'oldAlert(\'Completado\');'+
+            '};'+
+            'oldConsole.log(s);'+
+        '};'+
+        'sandbox.console = {'+
+           'log: function(s) {'+
+            'log(s);'+
+            '},'+
+        '};'+
+        'sandbox.document.write(filtrado);'+
+        'sandbox.document.close();'+
+        '}'+
+        ')')(escaper[nivel], input);
+    };
 };
 
 function cargarNivel(n) {
-    $('code').textContent = escaper[n];
-    nivel = n;
+    if (0 <= n && n < escaper.length) {
+        $('explicacion').textContent = '';
+        $('code').textContent = escaper[n];
+        nivel = n;
+    } else if (n == escaper.length) {
+        $('explicacion').innerHTML = "<h1>Haz terminado todos los niveles.</h1>";
+        nivel = 0;
+    } else {
+        cargarNivel(0);
+    };
+}
+
+function explicacion() {
+    $('explicacion').innerHTML = $(nivel).innerHTML;
 }
--- js/niveles.js
+++ js/niveles.js
@@ -1,5 +1,45 @@
 var escaper = [];
+
 escaper[0] = ""+
 "function escape(s) {\n"+
-"    return '<script>console.log(\"' + s +'\");</script>\'\n"+
+"   return '<script>console.log(\"' + s +'\");</script>\'\n"+
+"}";
+
+escaper[1] = ""+
+"function escape(s) {\n"+
+"   s = s.replace(/\"/g, '\\\\\"');\n"+
+"   return '<script>console.log(\"' + s + '\");</script>'\n"+
+"}";
+
+escaper[2] = ""+
+"function escape(s) {\n"+
+"   s = JSON.stringify(s);\n"+
+"   return '<script>console.log(' + s + ');</script>';\n"+
+"}";
+
+escaper[3] = ""+
+"function escape(s) {\n"+
+"   var url = 'javascript:console.log('+ JSON.stringify(s) +')';\n"+
+"   console.log(url);\n"+
+"\n"+
+"   var a = document.createElement('a');\n"+
+"   a.href = url;\n"+
+"   document.body.appendChild(a);\n"+
+"   a.click();\n"+
+"}";
+
+escaper[4] = ""+
+"function escape(s) {\n"+
+"   var text = s.replace(/</g, '&lt;').replace('\"', '&quot;');\n"+
+"   text = text.replace(/(http:\\\/\\\/\\\S+)/g, '<a href=\"$1\">$1</a>');\n"+
+"   text = text.replace(/\\\[\\\[(\\\w+)\\|(.+?)\\\]\\\]/g, '<img alt=\"$2\" src=\"$1.gif\">');\n"+
+"   return text;\n"+
+"}";
+
+escaper[5] = ""+
+"function escape(s) {\n"+
+"   var text = s.replace(/</g, '&lt;').replace('/\"/g', '&quot;');\n"+
+"   text = text.replace(/(http:\\\/\\\/\\\S+)/g, '<a href=\"$1\">$1</a>');\n"+
+"   text = text.replace(/\\\[\\\[(\\\w+)\\|(.+?)\\\]\\\]/g, '<img alt=\"$2\" src=\"$1.gif\">');\n"+
+"   return text;"+
 "}";
